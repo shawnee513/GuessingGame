7e5abde1934f77ffbae99c3a0f6bf12ab32d1d03
shawnee <shanpan513@gmail.com>
2022-08-26 12:51:33 -0600

    refactored ResultFragment to be composable. Started on GameFragment

--- app/src/main/java/com/example/guessinggame/GameFragment.kt
+++ app/src/main/java/com/example/guessinggame/GameFragment.kt
@@ -6,12 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.guessinggame.databinding.FragmentGameBinding
 import java.util.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.findNavController
 
 class GameFragment : Fragment() {
     private lateinit var binding: FragmentGameBinding
@@ -22,7 +32,15 @@ class GameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentGameBinding.inflate(inflater)
+        binding = FragmentGameBinding.inflate(inflater).apply {
+            gameCvComposeView.setContent {
+                MaterialTheme {
+                    Surface {
+                        GameFragmentContent(viewModel)
+                    }
+                }
+            }
+        }
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         //Set up dataBinding
@@ -55,4 +73,56 @@ class GameFragment : Fragment() {
 
         return binding.root
     }
+}
+@Composable
+fun FinishGameButton(clicked: () -> Unit) {
+    Button(onClick = clicked,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color(0xFF539987),
+            contentColor = Color(0xFFFFFFFF)
+        )) {
+        Text("END GAME")
+    }
+}
+
+@Composable
+fun EnterGuess(guess: String, changed: (String) -> Unit) {
+    TextField(
+        value = guess,
+        label = { Text("Guess a letter") },
+        onValueChange = changed,
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = Color(0xFFfaf9f9)
+        )
+    )
+}
+
+@Composable
+fun GuessButton(clicked: () -> Unit) {
+    Button(onClick = clicked,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color(0xFF539987),
+            contentColor = Color(0xFFFFFFFF)
+        )) {
+        Text("GUESS!")
+    }
+}
+
+@Composable
+fun GameFragmentContent(viewModel: GameViewModel) {
+    val guess = remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        EnterGuess(guess.value) { guess.value = it[0].toString() }
+    }
+
+    Column(modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+        GuessButton {
+            viewModel.makeGuess(guess.value)
+            guess.value = ""
+        }
+        FinishGameButton { viewModel.endGame()}
+    }
+
 }
--- app/src/main/java/com/example/guessinggame/ResultFragment.kt
+++ app/src/main/java/com/example/guessinggame/ResultFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.example.guessinggame.databinding.FragmentResultBinding
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import androidx.compose.ui.platform.ComposeView
 
 class ResultFragment : Fragment() {
-    private lateinit var binding: FragmentResultBinding
     private lateinit var viewModel: ResultViewModel
     private lateinit var viewModelFactory: ResultViewModelFactory
 
@@ -19,19 +28,49 @@ class ResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentResultBinding.inflate(inflater)
+        //code that doesn't use compose goes here
         val args = ResultFragmentArgs.fromBundle(requireArguments())
         viewModelFactory = ResultViewModelFactory(args.result)
         viewModel = ViewModelProvider(this, viewModelFactory).get(ResultViewModel::class.java)
 
-        //set the layout's data binding variable
-        binding.resultViewModel = viewModel
+        //return a compose view
+        return ComposeView(requireContext()).apply {
+            setContent {
+                //any compose code that defines the UI goes here
+                MaterialTheme {
+                    Surface {
+                        view?.let { ResultFragmentContent(it, viewModel) }
+                    }
+                }
+            }
+        }
+    }
+}
+@Composable
+fun ResultText(result: String) {
+    Text(text = result,
+        fontSize = 28 .sp,
+        textAlign = TextAlign.Center)
+}
 
-        //set onclick listeners
-        binding.resultBtPlayAgain.setOnClickListener {
-            findNavController().navigate(ResultFragmentDirections.actionResultFragmentToGameFragment())}
+@Composable
+fun NewGameButton(clicked: () -> Unit) {
+    Button(onClick = clicked,
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color(0xFF539987),
+                contentColor = Color(0xFFFFFFFF)
+            )) {
+        Text("PLAY AGAIN")
+    }
+}
 
-        // Inflate the layout for this fragment
-        return binding.root
+@Composable
+fun ResultFragmentContent(view: View, viewModel: ResultViewModel) {
+    Column(modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+        ResultText(viewModel.result.value.toString())
+        NewGameButton {
+            view.findNavController().navigate(R.id.action_resultFragment_to_gameFragment)
+        }
     }
 }
